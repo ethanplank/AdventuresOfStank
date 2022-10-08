24c6940bcaee492038b6ff45b7803ddb777ee4e5
ethanplank <82603228+ethanplank@users.noreply.github.com>
2022-10-08 14:21:17 -0400

    Camera follows player

---
+++ Adventures of Stank/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+    }
+}
