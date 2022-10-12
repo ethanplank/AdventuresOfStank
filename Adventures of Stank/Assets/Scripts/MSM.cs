5069a38e09224f0a44429d1376c15d9371d5a0f7
ethanplank <82603228+ethanplank@users.noreply.github.com>
2022-10-11 20:26:01 -0400

    Sprites update
    Added in sprites for sword in stone, hearts.  Implemented the ability to go to cave through door and it switches scenes.

--- Adventures of Stank/Assets/Scripts/MSM.cs
+++ Adventures of Stank/Assets/Scripts/MSM.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MSM : MonoBehaviour
 {
+    public int scene;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,27 @@ public class MSM : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void hitDoor()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+    public void hitSwordDoor()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            SceneManager.LoadScene(2);
+        }
+        if(SceneManager.GetActiveScene().buildIndex == 2)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+    public void pullSword()
+    {
+        //Enter code for animation of pulling sword out here.
+        //Then add sword to item slot
     }
 }
--- Adventures of Stank/Assets/Scripts/PlayerScript.cs
+++ Adventures of Stank/Assets/Scripts/PlayerScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     Rigidbody2D _rbody;
+   public MSM msm;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +17,16 @@ public class PlayerScript : MonoBehaviour
     {
         int speed = 5;
         _rbody.velocity = new Vector2(Input.GetAxis("Horizontal")*speed, Input.GetAxis("Vertical")*speed);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Door")
+        {
+            msm.hitDoor();
+        }
+        if(collision.gameObject.tag == "SwordDoor")
+        {
+            msm.hitSwordDoor();
+        }
     }
 }
