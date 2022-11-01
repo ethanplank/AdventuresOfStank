d110b5698fa7f8f278435a031754c9fbe2a0f7eb
STEIGERNA20 <82662750+STEIGERNA20@users.noreply.github.com>
2022-10-31 21:38:22 -0400

    Inventory pt.3 WIP

--- Adventures of Stank/Assets/Scripts/Item.cs
+++ Adventures of Stank/Assets/Scripts/Item.cs
@@ -11,4 +11,14 @@ public class Item
     }
     public ItemType itemType;
     public int amount;
+
+    public Sprite GetSprite()
+    {
+        switch(itemType)
+        {
+            default:
+            case ItemType.LaserSword: return ItemAssets.Instance.swordSprite;
+            case ItemType.LaserGun: return ItemAssets.Instance.gunSprite;
+        }
+    }
 }
