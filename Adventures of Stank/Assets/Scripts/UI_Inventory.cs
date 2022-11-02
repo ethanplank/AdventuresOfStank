e845d994ac338315eda354f032a3400cd506af1a
STEIGERNA20 <82662750+STEIGERNA20@users.noreply.github.com>
2022-11-01 21:42:47 -0400

    Inventory Error Fixed

--- Adventures of Stank/Assets/Scripts/UI_Inventory.cs
+++ Adventures of Stank/Assets/Scripts/UI_Inventory.cs
@@ -28,8 +28,8 @@ public class UI_Inventory : MonoBehaviour
             RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
             itemSlotRectTransform.gameObject.SetActive(true);
             itemSlotRectTransform.anchoredPosition = new Vector2(x * itemSlotCellSize, y * itemSlotCellSize);
-            Image image = itemSlotRectTransform.Find("image").GetComponent<Image>();
-            image.sprite = item.GetSprite();
+            //Image image = itemSlotRectTransform.Find("image").GetComponent<Image>();
+            //image.sprite = item.GetSprite();
             x++;
             
         }
