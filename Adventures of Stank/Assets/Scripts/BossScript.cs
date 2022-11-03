d1f1b19da8356c8b5c7b42227b4fbc054b07b223
ethanplank <82603228+ethanplank@users.noreply.github.com>
2022-11-02 22:45:04 -0400

    boss finish

--- Adventures of Stank/Assets/Scripts/BossScript.cs
+++ Adventures of Stank/Assets/Scripts/BossScript.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class BossScript : MonoBehaviour
 {
     public int health = 100;
+    public int speedx=4;
+    public int speedy = 3;
     public MSM msm;
+    public int time = 0;
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector2(speedx, speedy);
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        speedx = -speedx;
+        speedy=-speedy;
         if (collision.gameObject.tag == "Player")
         {
             msm.takeDamage(1);
