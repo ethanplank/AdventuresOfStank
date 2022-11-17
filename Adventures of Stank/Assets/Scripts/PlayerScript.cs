4a2945b07603b32af7ec4f4ed14a70495bd79672
STEIGERNA20 <82662750+STEIGERNA20@users.noreply.github.com>
2022-11-17 11:23:24 -0500

    mvmt?

--- Adventures of Stank/Assets/Scripts/PlayerScript.cs
+++ Adventures of Stank/Assets/Scripts/PlayerScript.cs
@@ -218,7 +218,7 @@ public class PlayerScript : MonoBehaviour
         }
         else
         {
-            direction = "still";
+            direction = direction;
         }
     }
     private void changeSkin()//Changing the animation based on direction
