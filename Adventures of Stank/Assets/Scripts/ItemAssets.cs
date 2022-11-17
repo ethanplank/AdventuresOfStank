5eb0efd4ea8f752a2c3deac26b6f341a2ae6d259
STEIGERNA20 <82662750+STEIGERNA20@users.noreply.github.com>
2022-11-17 09:47:24 -0500

    Comments for Inventory scripts

--- Adventures of Stank/Assets/Scripts/ItemAssets.cs
+++ Adventures of Stank/Assets/Scripts/ItemAssets.cs
@@ -6,11 +6,13 @@ public class ItemAssets : MonoBehaviour
 {
    public static ItemAssets Instance { get; private set; }
 
+    //sets Instance to what to what is being currently being used
     private void Awake()
     {
         Instance = this; 
     }
 
+    //Sprites for items
     public Sprite swordSprite;
     public Sprite gunSprite;
 }
