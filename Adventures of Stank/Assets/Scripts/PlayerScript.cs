a56ad63c6255b1c3e613cb747991ebc2a54fbbe9
STEIGERNA20 <82662750+STEIGERNA20@users.noreply.github.com>
2022-12-12 17:44:34 -0500

    Gun Pull Animation

--- Adventures of Stank/Assets/Scripts/PlayerScript.cs
+++ Adventures of Stank/Assets/Scripts/PlayerScript.cs
@@ -27,6 +27,7 @@ public class PlayerScript : MonoBehaviour
     const string gunForward = "StankGunForward";
 
     const string pullsword = "StankSwordPull 0";
+    const string pullGun = "StankGunPull";
 
 
     public string direction;
@@ -170,6 +171,7 @@ public class PlayerScript : MonoBehaviour
             if (msm.hasGun == 0)
             {
                 msm.PullGun();
+                pullGunAnimation();
             }
 
         }
@@ -413,6 +415,17 @@ public class PlayerScript : MonoBehaviour
         Invoke("TurnOffSword", 4);
         
 
+    }
+    public void pullGunAnimation()
+    {
+        animate.speed = 1;
+
+        isAttacking = true;
+        animate.Play(pullGun);
+
+        Invoke("StopAnim", 3);
+
+        Invoke("TurnOffGun", 3);
     }
   public void addSpeed()
     {
